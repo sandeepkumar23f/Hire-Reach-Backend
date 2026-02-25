9455325958ab12efc9c2df11e66ff4a684ed93df
sandeepkumar23f <sandeep21000552@gmail.com>
2026-02-25 17:47:13 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import { connection } from "./config/dbconfig.js"
+import authRoutes from "./routes/authRoutes.js"
+dotenv.config()
+const app = express()
+const allowedOrigins = [
+  "http://localhost:3000",
+];
+
+const port = process.env.PORT || 5000;
+
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json())
+app.use(cookieParser())
+connection()
+app.use("/api/auth",authRoutes)
+app.listen(port,(req,res)=>{
+    console.log(`app is running on port ${port}`)
+})
